aaf2d0f7620453054f03d099c7158da4a5970a0c
Olesia Konashchuk <konaschukl@gmail.com>
2026-01-17 23:41:25 +0100

    Update errorHandler.js

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,6 +1,17 @@
+import {HttpError} from 'http-errors';
+
 export const errorHandler = (err, req, res, next) =>{
   console.error('Error', err.message);
+
+   if (err instanceof HttpError) {
+    return res.status(err.status).json({
+      message: err.message || err.name,
+    });
+  }
+
+  const isProd = process.env.NODE_ENV === "production";
+
   res.status(500).json({
-    message: err.message,
+    message: isProd ? "Something went wrong. Please try again later." : err.message,
   });
 };
