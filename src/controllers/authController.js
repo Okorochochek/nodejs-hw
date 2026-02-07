69bd9fa81ac51f4985ffd2ad68589df63f400c27
Olesia Konashchuk <konaschukl@gmail.com>
2026-02-07 20:49:49 +0100

    fix

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -158,6 +158,6 @@ export const resetPassword = async (req, res, next) => {
   await Session.deleteMany({ userId: user._id });
 
   res.status(200).json({
-    message: 'Password reset successfully',
+    message: 'Password reset email sent successfully',
   });
 };
